c63b0e41c8aa1b6d191e885d1c181f75554a5ecd
GalloGabriel <gabriel.gallo2242@gmail.com>
2022-03-07 13:22:39 -0300

    atualizando js passoapasso

--- javascripts/teste2.js
+++ javascripts/teste2.js
@@ -2,31 +2,37 @@
 const arrayTextos = new Request('./data/exe2.json');
 let setaDireita = document.querySelector('#setaDireita');
 let setaEsquerda = document.querySelector('#setaEsquerda');
-let click = 1;
+let click = 0;
+let numTextos = 20;
 let json = {}
 let caixaDoMeio = document.querySelector(".exe2Container");
-let fazer = document.querySelector("#fazer")
-let naoFazer = document.querySelector("#nao-fazer")
+let fazer = document.querySelector("#fazer");
+let naoFazer = document.querySelector("#nao-fazer");
 let btnVerifica = document.querySelector("#exe2");
 
 //Setters
-setaDireita.addEventListener('click', fSetaDireita)
-setaEsquerda.addEventListener('click', fSetaEsquerda)
-btnVerifica.addEventListener('click', fVerifica)
-
+setaDireita.addEventListener('click', fSetaDireita);
+setaEsquerda.addEventListener('click', fSetaEsquerda);
+btnVerifica.addEventListener('click', fVerifica);
 
 
 //Funções
 
 function fVerifica(){
-  json.map(item =>{
-    if(item.devoFazerGab == item.devoFazer){
-      fCorreto(item.id)
-    }
-    else{
-      fErrado(item.id)
-    }
-  })
+  if(click === numTextos-1){
+    json.map(item =>{
+      if(item.devoFazerGab == item.devoFazer){
+        fCorreto(item.id)
+      }
+      else{
+        fErrado(item.id)
+      }
+    })
+  }else{
+    alert("Preencha as colunas antes de conferir o resultado")
+  }
+  
+  
 }
 function fCorreto(idjson){
   let elemento = document.querySelector('div[data-id="' + idjson + '"] > img')
@@ -46,31 +52,50 @@ function primeiroPasso(){
 }
 
 function fSetaDireita(texto){
-  if(click < 22){
-    apagarCaixaDoMeio()
-    atualizaJsonDevoFazer(click-1, false)
-    let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2');
-    caixaDoMeio.appendChild(elemento1);
+  if(click < numTextos){
 
-
-    let elemento2 = criaElemento(json[click-1].texto, 'card-teste-exe2');
-    elemento2.dataset.id = json[click-1].id
+    let elemento2 = criaElemento(json[click].texto, 'card-teste-exe2-coluna');
+    elemento2.dataset.id = json[click].id;
     naoFazer.appendChild(elemento2);
-    adicionaClick()
+
+    if(click < numTextos-1){
+      adicionaClick();
+      apagarCaixaDoMeio();
+      atualizaJsonDevoFazer(click, false);
+      let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2');
+      caixaDoMeio.appendChild(elemento1);
+    }else{
+      setaDireita.style.display = "none";
+      setaEsquerda.style.display = "none";
+      caixaDoMeio.style.display = "none";
+    }    
+    
   }
 }
 
 function fSetaEsquerda(texto){
-  if(click < 22){
-    apagarCaixaDoMeio()
-    atualizaJsonDevoFazer(click-1, true)
-    let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2');
-    caixaDoMeio.appendChild(elemento1);
-
-    let elemento2 = criaElemento(json[click-1].texto, 'card-teste-exe2');
+  if(click < numTextos){
+    //Construção do elemento 
+    let elemento2 = criaElemento(json[click].texto, 'card-teste-exe2-coluna');
     fazer.appendChild(elemento2);
-    elemento2.dataset.id = json[click-1].id
-    adicionaClick()
+    elemento2.dataset.id = json[click].id;
+
+    
+
+    if(click < numTextos-1){
+      //atualiza o contador
+      adicionaClick();
+      //atualiza caixa do meio
+      apagarCaixaDoMeio();
+      atualizaJsonDevoFazer(click, true);
+      let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2');
+      caixaDoMeio.appendChild(elemento1);  
+    }else{
+      setaDireita.style.display = "none";
+      setaEsquerda.style.display = "none";
+      caixaDoMeio.style.display = "none";
+    }
+    
   }
 }
 
@@ -80,8 +105,8 @@ function criaElemento(texto, classe){
   let p = document.createElement('p');
   div.appendChild(p);
   let img = document.createElement('img')
-  img.style.width = '15px'
-  img.style.height = '15px'
+  img.style.width = '35px'
+  img.style.height = '35px'
   img.style.display = 'none'
   div.appendChild(img);
   p.innerHTML = texto;
@@ -108,6 +133,7 @@ function adicionaClick(){
 }
 
 function atualizaJsonDevoFazer(i, value){
+  //console.log(json, click, 'i: ' + i)
   json[i].devoFazer = value;
 }
 
