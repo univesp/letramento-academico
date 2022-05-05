5df842bd4ba512e35588034bd52317b563380c76
GalloGabriel <gabriel.gallo2242@gmail.com>
2022-05-05 17:53:09 -0300

    ajustando exercicio 2 - pagina 4

--- javascripts/teste4_2.js
+++ javascripts/teste4_2.js
@@ -1,182 +1,101 @@
-function collapseTesteExe2(e){
-  let alternativas1 = document.getElementById('alternativas1-exe2');
-  let alternativas2 = document.getElementById('alternativas2-exe2');
-  let alternativas3 = document.getElementById('alternativas3-exe2');
+let select1Exe2 = document.getElementById('alternativas1-exe2');
+let select2Exe2 = document.getElementById('alternativas2-exe2');
+let select3Exe2 = document.getElementById('alternativas3-exe2');
 
-  let arrowText = document.getElementById('expand-teste1-exe2').innerText;
-  let arrow = document.getElementById('expand-teste1-exe2');
-  let arrowText2 = document.getElementById('expand-teste2-exe2').innerText;
-  let arrow2 = document.getElementById('expand-teste2-exe2');
-  let arrowText3 = document.getElementById('expand-teste3-exe2').innerText;
-  let arrow3 = document.getElementById('expand-teste3-exe2');
-  
+let imagem1Exe2 = document.getElementById('resultadoImg1-exe2');
+let imagem2Exe2 = document.getElementById('resultadoImg2-exe2');
+let imagem3Exe2 = document.getElementById('resultadoImg3-exe2');
 
-  if(e === 1 && arrowText === "arrow_drop_down"){
-    arrow.innerHTML = "arrow_drop_up"
-    alternativas1.style.display = "block";
-  }else{
-    arrow.innerHTML = "arrow_drop_down";
-    alternativas1.style.display = "none";
-  }
-
-  if(e === 2 && arrowText2 === "arrow_drop_down"){
-    arrow2.innerHTML = "arrow_drop_up"
-    alternativas2.style.display = "block";
-  }else{
-    arrow2.innerHTML = "arrow_drop_down";
-    alternativas2.style.display = "none";
-  }
-
-  if(e === 3 && arrowText3 === "arrow_drop_down"){
-    arrow3.innerHTML = "arrow_drop_up"
-    alternativas3.style.display = "block";
-  }else{
-    arrow3.innerHTML = "arrow_drop_down";
-    alternativas3.style.display = "none";
-  }
- 
-}
-
-function insereTextoExe2(e){
-  const option1 = "Citação indireta";
-  const option2 = "Citação direta com mais de 3 linhas";
-  const option3 = "Citação direta com menos de 3 linhas";
-
-  let paragrafo1Div1 = document.getElementById('texto-option2-div1');
-  let paragrafo1Div2 = document.getElementById('texto-option2-div2');
-  let paragrafo1Div3 = document.getElementById('texto-option2-div3');
-
-  let alternativas1 = document.getElementById('alternativas1-exe2');
-  let alternativas2 = document.getElementById('alternativas2-exe2');
-  let alternativas3 = document.getElementById('alternativas3-exe2');
-
-  let arrow = document.getElementById('expand-teste1-exe2');
-  let arrow2 = document.getElementById('expand-teste2-exe2');
-  let arrow3 = document.getElementById('expand-teste3-exe2');
-
-  if(e === 1){
-    paragrafo1Div1.innerHTML = option1;
-    alternativas1.style.display = "none";
-    arrow.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 2){
-    paragrafo1Div1.innerHTML = option2;
-    alternativas1.style.display = "none";
-    arrow.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 3){
-    paragrafo1Div1.innerHTML = option3;
-    alternativas1.style.display = "none";
-    arrow.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 4){
-    paragrafo1Div2.innerHTML = option1;
-    alternativas2.style.display = "none";
-    arrow2.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 5){
-    paragrafo1Div2.innerHTML = option2;
-    alternativas2.style.display = "none";
-    arrow2.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 6){
-    paragrafo1Div2.innerHTML = option3;
-    alternativas2.style.display = "none";
-    arrow2.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 7){
-    paragrafo1Div3.innerHTML = option1;
-    alternativas3.style.display = "none";
-    arrow3.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 8){
-    paragrafo1Div3.innerHTML = option2;
-    alternativas3.style.display = "none";
-    arrow3.innerHTML = "arrow_drop_down";
-  }
-
-  if(e === 9){
-    paragrafo1Div3.innerHTML = option3;
-    alternativas3.style.display = "none";
-    arrow3.innerHTML = "arrow_drop_down";
-  }
-}
 
 function geraResultado2(){
-  const option1 = "Citação indireta";
-  const option2 = "Citação direta com mais de 3 linhas";
-  const option3 = "Citação direta com menos de 3 linhas";
-  const vazio = "Escolha um tipo de citação";
 
-  let paragrafo1Div1 = document.getElementById('texto-option2-div1').innerText;
-  let paragrafo1Div2 = document.getElementById('texto-option2-div2').innerText;
-  let paragrafo1Div3 = document.getElementById('texto-option2-div3').innerText;
+  let valueSelected1Exe2 = document.getElementById('alternativas1-exe2').value;
+  let valueSelected2Exe2 = document.getElementById('alternativas2-exe2').value;
+  let valueSelected3Exe2 = document.getElementById('alternativas3-exe2').value;
+  
+  if(valueSelected1Exe2 !== "" && valueSelected2Exe2 !== "" && valueSelected3Exe2 !== ""){
 
-  let resultadoDiv1 = document.getElementById('resultado-div1');
-  let resultadoDiv2 = document.getElementById('resultado-div2');
-  let resultadoDiv3 = document.getElementById('resultado-div3');
+    //Mostra os icones de certo ou errado
+    let iconesResultado = document.querySelectorAll('.resultado-teste-secao4');
 
-  let imagem1 = document.getElementById('resultadoImg1');
-  let imagem2 = document.getElementById('resultadoImg2');
-  let imagem3 = document.getElementById('resultadoImg3');
+    iconesResultado.forEach(e => {
+      e.style.display = "block";
+    })
 
-  let arrow = document.getElementById('expand-teste1-exe2');
-  let arrow2 = document.getElementById('expand-teste2-exe2');
-  let arrow3 = document.getElementById('expand-teste3-exe2');
+    //Seta os icones de certo ou errado de acordo com a resposta
+    if(valueSelected1Exe2 === "option1"){
+      imagem1Exe2.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem1Exe2.setAttribute('src', '/assets/13_errado.svg');
+    }
 
-  let selectDiv = document.querySelectorAll('.select-teste2-secao4');
+    if(valueSelected2Exe2 === "option5"){
+      imagem2Exe2.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem2Exe2.setAttribute('src', '/assets/13_errado.svg');
+    }
 
-  let botaoResultado = document.getElementById('botao-exe4');
-  let botaoTexto = document.getElementById('botao-exe4').innerText;
+    if(valueSelected3Exe2 === "option9"){
+      imagem3Exe2.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem3Exe2.setAttribute('src', '/assets/13_errado.svg');
+    }
 
+    let expandIconExe2 = document.querySelectorAll('.alternativas-exe4');
 
-  if(paragrafo1Div1 === vazio || paragrafo1Div2 === vazio || paragrafo1Div3 === vazio){
+    expandIconExe2.forEach(e => {
+      e.style.backgroundImage = "none";
+    })
+
+    //mosttra as respostas corretas
+    let resultadoFrases = document.querySelectorAll('.resultado-exercicio4-2');
+
+    resultadoFrases.forEach(e => {
+      e.style.display = "block";
+    })
+
+    //ajusta posicionamento dos selects depois de aparecer as respostas
+    let selectAfter1 = document.getElementById('selectExe4Box1');
+    let selectAfter2 = document.getElementById('selectExe4Box2');
+    let selectAfter3 = document.getElementById('selectExe4Box3');
+
+    selectAfter1.style.marginTop = "-40px";
+    selectAfter2.style.marginTop = "-40px";
+    selectAfter3.style.marginTop = "15px";
+
+    //desabilitando botão depois de finalizar o exercício
+    let botao = document.querySelector('#botao-exe4');
+    botao.disabled = true;
+    botao.style.cursor = "default";
+
+  }else{
     alert("Por favor, selecione todas as respostas.");
+  }
+}
+
+// alterando a cor de fundo dos selects
+function toggleValueSelect2(){
+  
+  let valueSelected1Exe2 = document.getElementById('alternativas1-exe2').value;
+  let valueSelected2Exe2 = document.getElementById('alternativas2-exe2').value;
+  let valueSelected3Exe2 = document.getElementById('alternativas3-exe2').value;
+
+  if(valueSelected1Exe2 !== ""){
+    select1Exe2.style.backgroundColor = "#ff9cff";
   }else{
-    resultadoDiv1.style.display = "block";
-    resultadoDiv2.style.display = "block";
-    resultadoDiv3.style.display = "block";
-
-    selectDiv.forEach(element =>{
-      element.style.backgroundColor = "#ff9cff";
-      element.style.cursor = "auto";
-    });
-
-    arrow.style.display = "none";
-    arrow2.style.display = "none";
-    arrow3.style.display = "none";
-
-    botaoResultado.innerHTML = "Tentar novamente";
+    select1Exe2.style.backgroundColor = "#f2f2f2";
   }
 
-  if(paragrafo1Div1 === option1){
-    imagem1.setAttribute('src', '/assets/12_certo.svg');
+  if(valueSelected2Exe2 !== ""){
+    select2Exe2.style.backgroundColor = "#ff9cff";
   }else{
-    imagem1.setAttribute('src', '/assets/13_errado.svg');
+    select2Exe2.style.backgroundColor = "#f2f2f2";
   }
 
-  if(paragrafo1Div2 === option2){
-    imagem2.setAttribute('src', '/assets/12_certo.svg');
+  if(valueSelected3Exe2 !== ""){
+    select3Exe2.style.backgroundColor = "#ff9cff";
   }else{
-    imagem2.setAttribute('src', '/assets/13_errado.svg');
+    select3Exe2.style.backgroundColor = "#f2f2f2";
   }
-
-  if(paragrafo1Div3 === option3){
-    imagem3.setAttribute('src', '/assets/12_certo.svg');
-  }else{
-    imagem3.setAttribute('src', '/assets/13_errado.svg');
-  }
-
-
-  if(botaoTexto === "Tentar novamente"){
-    document.location.reload(true);
-  }
-
+  
 }
