2b5f15d54ef27f7841aa0685408302aa79bbf57c
GalloGabriel <gabriel.gallo2242@gmail.com>
2022-05-05 12:46:31 -0300

    ajustes exercicio 4

--- javascripts/teste4.js
+++ javascripts/teste4.js
@@ -4,6 +4,11 @@ let select2 = document.getElementById('alternativas2');
 let select3 = document.getElementById('alternativas3');
 let select4 = document.getElementById('alternativas4');
 
+let imagem1 = document.getElementById('resultadoImg1');
+let imagem2 = document.getElementById('resultadoImg2');
+let imagem3 = document.getElementById('resultadoImg3');
+let imagem4 = document.getElementById('resultadoImg4');
+
 function geraResultado(){
   let resultado = document.querySelector('.resultado-secao4');
   let valueSelected1 = document.getElementById('alternativas1').value;
@@ -11,11 +16,52 @@ function geraResultado(){
   let valueSelected3 = document.getElementById('alternativas3').value;
   let valueSelected4 = document.getElementById('alternativas4').value;
 
-  if(valueSelected1 !== "" || valueSelected2 !== "" || valueSelected3 !== "" || valueSelected4 !== ""){
+  if(valueSelected1 !== "" && valueSelected2 !== "" && valueSelected3 !== "" && valueSelected4 !== ""){
     resultado.style.display = "block";
+    imagem1.style.display = "block";
+    imagem2.style.display = "block";
+    imagem3.style.display = "block";
+    imagem4.style.display = "block";
+
+    //desabilitando botão depois de finalizar o exercício
+    let botao = document.querySelector('.botao-resultado');
+    botao.disabled = true;
+    botao.style.cursor = "default";
+
+    let expandIcon = document.querySelectorAll('.open-secao4');
+
+    expandIcon.forEach(e => {
+      e.style.display = "none";
+    })
+
+    //Seta os icones de certo ou errado de acordo com a resposta
+    if(valueSelected1 === "option2"){
+      imagem1.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem1.setAttribute('src', '/assets/13_errado.svg');
+    }
+
+    if(valueSelected2 === "option8"){
+      imagem2.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem2.setAttribute('src', '/assets/13_errado.svg');
+    }
+
+    if(valueSelected3 === "option11"){
+      imagem3.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem3.setAttribute('src', '/assets/13_errado.svg');
+    }
+
+    if(valueSelected4 === "option13"){
+      imagem4.setAttribute('src', '/assets/12_certo.svg');
+    }else{
+      imagem4.setAttribute('src', '/assets/13_errado.svg');
+    }
+
   }else{
     alert("Por favor, selecione todas as respostas.");
-  }
+  }  
 }
 
 // alterando a cor de fundo dos selects
@@ -48,6 +94,39 @@ function toggleValueSelect(){
     select4.style.backgroundColor = "#fff";
   }else{
     select4.style.backgroundColor = "#f2f2f2";
+  }
+  
+}
+
+function boldSelected(){
+  let valueSelected2 = document.getElementById('alternativas2').value;
+
+  if(valueSelected2 === "option7" || valueSelected2 === "option8"){
+    select2.style.fontWeight = "bold";
+  }else{
+    select2.style.fontWeight = "normal";
+  }
+  
+}
+
+function boldSelected2(){
+  let valueSelected3 = document.getElementById('alternativas3').value;
+
+  if(valueSelected3 === "option9" || valueSelected3 === "option10"){
+    select3.style.fontWeight = "bold";
+  }else{
+    select3.style.fontWeight = "normal";
+  }
+  
+}
+
+function boldSelected3(){
+  let valueSelected4 = document.getElementById('alternativas4').value;
+
+  if(valueSelected4 === "option15" || valueSelected4 === "option16"){
+    select4.style.fontWeight = "bold";
+  }else{
+    select4.style.fontWeight = "normal";
   }
   
 }
