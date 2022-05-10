0835e5e602c21f8dbb82a542180a444f490ecca3
Gabriel Henrique Gallo da Silva <gabrielsilva@fpanet.inet>
2022-05-10 15:14:50 -0300

    melhorando collapse pagina 1

--- javascripts/events.js
+++ javascripts/events.js
@@ -21,70 +21,43 @@ $(document).ready(function(){
 
 })
 
-/////TOGGLE DAS IMAGENS DOS LIVROS/////
-function setHover(id){
-  let livro1 = document.getElementById('livro1');
-  let livro2 = document.getElementById('livro2');
-  let livro3 = document.getElementById('livro3');
-  let livro4 = document.getElementById('livro4');
-
-  if(id === livro1){
-    livro1.setAttribute('src', '/assets/02_botaosecao01-hover.svg');
-  }
-  else if(id === livro2){
-    livro2.setAttribute('src', '/assets/03_botaosecao02-hover.svg');
-  }
-  else if(id === livro3){
-    livro3.setAttribute('src', '/assets/04_botaosecao03-hover.svg');
-  }
-  else if(id === livro4){
-    livro4.setAttribute('src', '/assets/05_botaosecao04-hover.svg');
-  }
-  
-}
-
-function leaveHover(id){
-  let livro1 = document.getElementById('livro1');
-  let livro2 = document.getElementById('livro2');
-  let livro3 = document.getElementById('livro3');
-  let livro4 = document.getElementById('livro4');
-
-  if(id === livro1){
-    livro1.setAttribute('src', '/assets/02_botaosecao01.svg');
-  }
-  else if(id === livro2){
-    livro2.setAttribute('src', '/assets/03_botaosecao02.svg');
-  }
-  else if(id === livro3){
-    livro3.setAttribute('src', '/assets/04_botaosecao03.svg');
-  }
-  else if(id === livro4){
-    livro4.setAttribute('src', '/assets/05_botaosecao04.svg');
-  }
-  
-}
-/////FIM TOGGLE DAS IMAGENS DOS LIVROS/////
-
 
 function toggleExpand(){
   let expand01 = document.getElementById('expand1');
   let spanTexto = document.getElementById('expand1').innerText;
+  let expand02 = document.getElementById('expand2');
+  let spanTexto2 = document.getElementById('expand2').innerText;
 
   if(spanTexto === "expand_more"){
     expand01.innerHTML = "expand_less";
   }else{
     expand01.innerHTML = "expand_more";
   }  
+
+  if(spanTexto2 === "expand_less"){
+    expand02.innerHTML = "expand_more";
+  }  
 }
 
 function toggleExpand2(){
   let expand02 = document.getElementById('expand2');
   let spanTexto2 = document.getElementById('expand2').innerText;
-
+  let expand01 = document.getElementById('expand1');
+  let spanTexto = document.getElementById('expand1').innerText;
+  
   if(spanTexto2 === "expand_more"){
     expand02.innerHTML = "expand_less";
+
+    //scrolla pagina para inicio do teste depois de conferir resultado
+    window.scrollTo(0, 2200);
   }else{
     expand02.innerHTML = "expand_more";
-  }  
+  } 
+  
+  if(spanTexto === "expand_less"){
+    expand01.innerHTML = "expand_more";
+  }
+
+  
 }
 
