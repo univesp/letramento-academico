7ff240aab623fe9efbbaaeef6de5601bc8d3446b
Alberto Ortiz Mendes <35493648+AlbertoOrtizMendes@users.noreply.github.com>
2022-05-13 14:36:15 -0300

    Ajustes na dinâmica do click
    Ajustes na dinâmica do click

--- javascripts/teste2.js
+++ javascripts/teste2.js
@@ -1,129 +1,98 @@
-// Variáveis/elementos
+// Variáveis globais
 const arrayTextos = new Request('./data/exe2.json');
-let setaDireita = document.querySelector('#setaDireita');
-let setaEsquerda = document.querySelector('#setaEsquerda');
 let click = 0;
 let numTextos = 20;
+let newProgress = 0;
+let baseURL = 'https://apps.univesp.br/letramento-academico/assets/';
+
+//Getters
+let setaDireita = document.querySelector('#setaDireita');
+let setaEsquerda = document.querySelector('#setaEsquerda');
 let json = {}
 let caixaDoMeio = document.querySelector(".exe2Container");
 let resultado = document.querySelector(".exe2-container");
 let fazer = document.querySelector("#fazer");
 let naoFazer = document.querySelector("#nao-fazer");
 let btnVerifica = document.querySelector("#exe2");
-let index = 0;
 let progress = document.querySelector('.progress');
-let newProgress = 0;
-let clickProgresso = 0;
 let $counter = $('.progress-bar');
-let numBoxCounter = 1;
-
-let baseURL = 'https://apps.univesp.br/letramento-academico/assets/';
-
-//Setters
-setaDireita.addEventListener('click', fSetaDireita);
-setaEsquerda.addEventListener('click', fSetaEsquerda);
-btnVerifica.addEventListener('click', fVerifica);
-
 
 //Funções
-
-function fVerifica(){
-  if(click === numTextos-1){
-    json.map(item =>{
-      
-      if(item.devoFazerGab === item.devoFazer){
-        fCorreto(item.id)
-      }
-      else{
-        fErrado(item.id)
-      }
-    })
-  }else{
-    alert("Preencha as colunas antes de conferir o resultado")
-  }
-  
-  
+let leJSON = function() {
+  fetch(arrayTextos)
+  .then(response => {
+    return(response.json());
+  }).then(data => {
+    json = data;
+    primeiroPasso()
+    return data
+  })
 }
-function fCorreto(idjson){
-  let elemento = document.querySelector('div[data-id="' + idjson + '"] > img')
-  elemento.src = baseURL+'12_certo.svg'
-  elemento.style.display = 'inherit'
-}
-function fErrado(idjson){
-  let elemento = document.querySelector('div[data-id="' + idjson + '"] > img')
-  elemento.src = baseURL+'13_errado.svg'
-  elemento.style.display = 'inherit'
-}
-
-function primeiroPasso(){
-  let texto = json[0].texto;
-  let elemento = criaElemento(texto, 'card-teste-exe2', '1','1/20');
-  caixaDoMeio.appendChild(elemento)
-}
-
-function fSetaDireita(texto){
+let fSeta = function(){
+  let isEsquerda = (this.id == "setaEsquerda")
+  //Dinâmica do Load
   newProgress = newProgress + 5;
-
   $counter.attr('aria-valuenow', newProgress+'%').css('width', newProgress+'%');
-  
-
+  //Click de 0 até 19
   if(click < numTextos){
+    //Pega o texto atual da caixa e joga pra caixa em qual clicou
 
+    //Cria o elemento
     let elemento2 = criaElemento(json[click].texto, 'card-teste-exe2-coluna', 'coluna-naoDevoFazer', ' ');
+    //Seta o id do objeto no elemento criado
     elemento2.dataset.id = json[click].id;
-    naoFazer.appendChild(elemento2);
+    //Appenda o elemento na caixa respectiva
+    if(isEsquerda){
+      fazer.appendChild(elemento2)
+    }
+    else{
+      naoFazer.appendChild(elemento2);
+    }
+    //Atualiza objeto
+    atualizaJsonDevoFazer(click, isEsquerda);
+    //atualiza o contador
+    adicionaClick();
 
-    if(click < numTextos-1){
-      adicionaClick();
+    //Atualiza caixa do meio se click  de 0 a 19
+    if(click < 20){
       apagarCaixaDoMeio();
-      atualizaJsonDevoFazer(index, false);
-      index++;
-      numBoxCounter++;
-      let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2', 'card-teste-exe2', String(numBoxCounter)+'/20');
+      let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2', 'card-teste-exe2', String(click+1)+'/20');
       caixaDoMeio.appendChild(elemento1);
-    }else{
-      setTimeout(() => {apagaSetaseCaixaDoMeio()}, 1000);
-      setTimeout(() => {mostraResultado()}, 1000);
-      setTimeout(() => {fVerifica()}, 1000);
-    }    
-    
-  }
-}
-
-function fSetaEsquerda(texto){
-  newProgress = newProgress + 5;
-
-  $counter.attr('aria-valuenow', newProgress+'%').css('width', newProgress+'%');
-  
-
-  if(click < numTextos){
-    //Construção do elemento 
-    let elemento2 = criaElemento(json[click].texto, 'card-teste-exe2-coluna', 'coluna-devoFazer', '');
-    fazer.appendChild(elemento2);
-    elemento2.dataset.id = json[click].id;
-
-    
-
-    if(click < numTextos-1){
-      //atualiza o contador
-      adicionaClick();
-      //atualiza caixa do meio
-      apagarCaixaDoMeio();
-      atualizaJsonDevoFazer(index, true);
-      index++;
-      numBoxCounter++;
-      let elemento1 = criaElemento(json[click].texto, 'card-teste-exe2', 'card-teste-exe2', String(numBoxCounter)+'/20');
-      caixaDoMeio.appendChild(elemento1);  
-    }else{
+    }
+    //se click = 20 revela o resultado
+    else{
       setTimeout(() => {apagaSetaseCaixaDoMeio()}, 1000);
       setTimeout(() => {mostraResultado()}, 1000);
       setTimeout(() => {fVerifica()}, 1000);
     }
-    
-  }
+  }  
 }
-
-function criaElemento(texto, classe, classe2, numBox){
+let primeiroPasso = function() {
+  let texto = json[0].texto;
+  let elemento = criaElemento(texto, 'card-teste-exe2', '1','1/20');
+  caixaDoMeio.appendChild(elemento)
+}
+let fVerifica = function() {
+  json.map(item =>{
+    if(item.devoFazerGab === item.devoFazer){
+      fCorreto(item.id)
+    }
+    else{
+      fErrado(item.id)
+    }
+  })
+}
+let fCorreto = function(idjson) {
+  let elemento = document.querySelector('div[data-id="' + idjson + '"] > img')
+  elemento.src = baseURL+'12_certo.svg'
+  elemento.style.display = 'inherit'
+}
+let fErrado = function(idjson) {
+  let elemento = document.querySelector('div[data-id="' + idjson + '"] > img')
+  elemento.src = baseURL+'13_errado.svg'
+  elemento.style.display = 'inherit'
+}
+let criaElemento = function(texto, classe, classe2, numBox) {
   let div = document.createElement('div');
   div.classList.add(classe);
   div.classList.add(classe2);
@@ -141,41 +110,29 @@ function criaElemento(texto, classe, classe2, numBox){
   span.innerHTML = numBox;
   return div
 }
-
-function leJSON(){
-  fetch(arrayTextos)
-  .then(response => {
-    return(response.json());
-  }).then(data => {
-    json = data;
-    primeiroPasso()
-    return data
-  })
-}
-
-function apagaSetaseCaixaDoMeio(){
+let apagaSetaseCaixaDoMeio = function() {
   setaDireita.style.display = "none";
   setaEsquerda.style.display = "none";
   caixaDoMeio.style.display = "none";
   progress.style.display = "none";
 }
-
-function apagarCaixaDoMeio(){
+let apagarCaixaDoMeio = function() {
   caixaDoMeio.innerHTML = ""
 }
-
-function adicionaClick(){
+let adicionaClick = function() {
   click++;
 }
-
-function atualizaJsonDevoFazer(i, value){
-  //console.log(json, click, 'i: ' + i)
-  
+let atualizaJsonDevoFazer = function(i, value) {
   json[i].devoFazer = value;
 }
-
-function mostraResultado(){
+let mostraResultado = function() {
   resultado.style.display = "flex";
 }
 
+//Setters
+setaDireita.addEventListener('click', fSeta);
+setaEsquerda.addEventListener('click', fSeta);
+btnVerifica.addEventListener('click', fVerifica);
+
+//Início do processamento
 leJSON()
